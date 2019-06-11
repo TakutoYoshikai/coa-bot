9c132bf0f23f5c6a8f005f17c52e3c28dd26479f
Takuto Yoshikai <takuto.yoshikai@gmail.com>
2019-06-11 12:36:38 +0900

    random talk

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ function loadConfig() {
   return JSON.parse(fs.readFileSync("config.json"));
 }
 
+function randomTalk() {
+  var choices = [
+    "今IBMのクラウド量子コンピュータを使ってRSA暗号を解読してる最中だから邪魔しないで",
+    "グラフ理論とベイズ統計を勉強中",
+    "ディープラーニングのlearning rateミスっていつまで経っても学習できないよー",
+    "マルウェア解析中。機械学習で悪意のあるプログラムを動的に検知できればと思ってね",
+    "ダークウェブに入って、最新のサイバー犯罪について調査している",
+    "ペンタゴンにハッキングして国家の機密情報盗んできた"
+  ]
+  var choice = choices[Math.floor(Math.random() * choices.length)];
+  return choice;
+}
+
 
 client.on("ready", function() {
   //client.channels.get(channels.general).send("よろしくね");
@@ -40,6 +53,9 @@ client.on("message", function(message) {
       message.reply("ありがとう！！今日のお小遣いもよろしくね！！！");
     }, 5000);
     
+  }
+  if (message.content == "何してるの") {
+    message.reply(randomTalk());
   }
 });
 
